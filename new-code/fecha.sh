1cdc3e3a3af9c0a13a737c9f662797a830f1e06d
Liquenson <liquensonrubenalexis@gmail.com>
2024-06-29 00:28:59 +0200

    Segunda tarea

---
+++ new-code/fecha.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Ingrese la fecha : "
+read fecha
+
+echo "Fecha: $fecha"
+
+sleep 2
+
+echo "Ingrese la hora : "
+read hora
+
+echo "La hora es: $hora"
+# Segunda tarea
--- new-code/usuario1.sh
+++ new-code/usuario1.sh
@@ -2,6 +2,6 @@
 
 echo "Ingresa tu nombre de usuario:"
 read  usuario
-echo "El nombre de usuario ingresado es: $usuario"
 
-# primera tarea.
+echo "El nombre de usuario ingresado es: $usuario"
+# Primera tarea.
