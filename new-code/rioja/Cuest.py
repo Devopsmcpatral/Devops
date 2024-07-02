9a46c2d8fdec6057afeb3506d2e6814754cfa35a
Liquenson <liquensonrubenalexis@gmail.com>
2024-07-02 21:45:45 +0200

    Datos

---
+++ new-code/rioja/Cuest.py
@@ -0,0 +1,17 @@
+
+nombre = input("¿Cuál es tu nombre? ")
+
+apellido = input("¿Cuál es tu apellido? ")
+
+edad = input("¿Cuántos años tienes? ")
+
+nacionalidad = input("¿Cuál es tu nacionalidad? ")
+
+datos_personales = input("Introduce algunos datos personales adicionales que desees compartir: ")
+
+print("\n--- Información recopilada ---")
+print(f"Nombre: {nombre}")
+print(f"Apellido: {apellido}")
+print(f"Edad: {edad}")
+print(f"Nacionalidad: {nacionalidad}")
+print(f"Datos personales adicionales: {datos_personales}")
