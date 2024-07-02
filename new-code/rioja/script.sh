04eb692450b11fc6dd6b9952c135eadd9a9e1dca
Liquenson <liquensonrubenalexis@gmail.com>
2024-07-03 00:04:39 +0200

    Nueva actualizacion

---
+++ new-code/rioja/script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "Ingrese su nombre:"
+read nombre
+
+sleep 2
+
+echo "Ingrese su edad:"
+read edad
+
+sleep 2
+
+echo "Ingrese su dirección:"
+read direccion
+
+sleep 2
+
+echo "Ha ingresado los siguientes datos:"
+echo "Nombre: $nombre"
+echo "Edad: $edad"
+echo "Dirección: $direccion"
